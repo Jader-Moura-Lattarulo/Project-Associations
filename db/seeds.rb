1bde8eca833b2a976588ec76f27091d2855b6ef5
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-22 17:46:10 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+doctor01 = Doctor.create(name: "Stephen Vincent")
+doctor02 = Doctor.create(name: "Bruce Banner")
+doctor03 = Doctor.create(name: "Jonathan Osterman")
+
+patient01 = Patient.create(name: "Estranho")
+patient02 = Patient.create(name: "Hulk")
+patient03 = Patient.create(name: "Manhattan")
+
+Appointment.create(doctor: doctor01, patient: patient01, date: Date.today)
+Appointment.create(doctor: doctor02, patient: patient02, date: Date.today)
+Appointment.create(doctor: doctor03, patient: patient03, date: Date.today)
+Appointment.create(doctor: doctor01, patient: patient03, date: Date.today)
+Appointment.create(doctor: doctor02, patient: patient01, date: Date.today)
+Appointment.create(doctor: doctor03, patient: patient02, date: Date.today)
